5644909f1b2c0985f46f0cdc9302e71f43190fbd
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2021-02-02 12:00:40 +0800

    commit for heroku

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :glass, Glass.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :glass, GlassWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 3000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
