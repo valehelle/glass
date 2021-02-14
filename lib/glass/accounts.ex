54d0f1665001f41d64b3434ef0f483888bf58449
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2021-02-14 19:13:04 +0800

    edit UI

--- lib/glass/accounts.ex
+++ lib/glass/accounts.ex
@@ -269,7 +269,7 @@ defmodule Glass.Accounts do
   """
   def get_user_by_session_token(token) do
     {:ok, query} = UserToken.verify_session_token_query(token)
-    Repo.one(query) |> Repo.preload([:basic, :educations, :languages, :projects, :works, :skills, :blogs])
+    Repo.one(query) |> Repo.preload([:basic, :educations, :languages, :works, :skills, :blogs, projects: [:keywords]])
   end
 
   @doc """
--- lib/glass_web/views/dashboard_view.ex
+++ lib/glass_web/views/dashboard_view.ex
@@ -6,5 +6,30 @@ defmodule GlassWeb.DashboardView do
       nil -> ""
     end
   end
+
+  def get_language(language) do
+    profiencies = [
+      "Beginner",
+      "Intermediate",
+      "Proficient",
+    ]
+    {language, _} = List.pop_at(profiencies, language - 1)
+    language
+  end
+
+
+  def get_skill(skill) do
+    profiencies = [
+      "Daily experience",
+      "Experienced",
+      "Used in the past",
+      "Interests and Curiosity"
+    ]
+    {skill, _} = List.pop_at(profiencies, skill - 1)
+    skill
+  end
+
+
+
   
 end
