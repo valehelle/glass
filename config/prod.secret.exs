ff337095041114b90a49f7443091b418fa8ab06d
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2021-02-18 06:59:28 +0800

    edit prod

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -15,7 +15,7 @@ config :glass, Glass.Repo,
   ssl: true,
   url: database_url,
   send_grid_token: System.get_env("send_grid_token"),
-  github_client_secret: System.get_env("send_grid_token"),
+  github_client_secret: System.get_env("github_client_secret"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
